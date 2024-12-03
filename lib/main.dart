96446e7c63be112b187e74ea88ac8097a5f10ae6
Dmitry <ddl.star@yandex.ru>
2024-12-03 18:14:38 +0300

    подключение бэкенда

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/auth/login_or_register.dart';
 import 'package:food_app/models/restaurant.dart';
 import 'package:food_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.collection('test').add({'test': 'test'});
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => ThemeProvider()),
